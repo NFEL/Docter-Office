4cb23473bf2a8650485bef1c7e70e4a6feeb9b0e
Ali Sayyadi <as.alisayyadi@gmail.com>
2022-02-01 22:15:52 +0330

    weekdays

--- Models/Doctor.py
+++ Models/Doctor.py
@@ -1,9 +1,12 @@
+from calendar import MONDAY, SATURDAY
 from datetime import datetime
+from typing import Dict
 from pydantic import BaseModel
+from Weekdays import Weekdays
 
 class Doctor(BaseModel):
     first_name: str
     last_name: str
     specialty: str
     address: str
-    present_time: datetime
+    present_time: Dict[Weekdays,datetime]
---
+++ Models/Weekdays.py
@@ -0,0 +1,10 @@
+from enum import Enum
+class Weekdays(Enum):
+    SAT = "Saturday"
+    SUN = "Sunday"
+    MON = "Monday"
+    TEU = "Teusday"
+    WED = "Wednesday"
+    THU = "Thursday"
+    FRI = "Friday"
+
